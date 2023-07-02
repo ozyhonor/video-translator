de2f1fc1abf90eb4152291ed5718c5f29e899c1c
Евгений Дубровский <dubrovskij-00@inbox.ru>
2023-07-02 08:30:20 +0300

    draft

--- draft.py
+++ draft.py
@@ -1,1 +1,15 @@
-...
+import configparser 
+import sys
+
+
+if __name__ == "__main__":
+    
+    # Чтение данных из INI-файла
+    try:
+        config = configparser.ConfigParser()
+        config.read('./secret/master-kqys.ini')
+        #print(config['a'])
+    except: 
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        print(f"Произошла ошибка типа: {exc_type.__name__},  {exc_traceback}")
+
